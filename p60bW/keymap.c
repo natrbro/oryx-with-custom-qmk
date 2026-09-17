150e8b728f8511b072138d2b2bc910920e1a87aa
Nick Rohleder <natrbro@google.com>
2026-09-16 17:20:43 -0700

    Try to get mod-tap to work for caps word

--- p60bW/keymap.c
+++ p60bW/keymap.c
@@ -16,7 +16,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_voyager(
     KC_GRAVE,       KC_1,           KC_2,           KC_3,           KC_4,           KC_5,                                           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,       
     KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                                           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLS,        
-    KC_LEFT_SHIFT,  KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        MT(MOD_RSFT, KC_QUOTE),
+    MT(MOD_LSFT, KC_F24),KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        MT(MOD_RSFT, KC_QUOTE),
     KC_MS_BTN1,     MT(MOD_LCTL, KC_Z),MT(MOD_LALT, KC_X),MT(MOD_LSFT, KC_C),KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         MT(MOD_RALT, KC_SLASH),KC_TRANSPARENT, 
                                                     MT(MOD_LGUI, KC_ENTER),MT(MOD_LCTL, KC_TAB),                                KC_BSPC,        LT(1, KC_SPACE)
   ),
@@ -159,6 +159,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   }
 
   switch (keycode) {
+  case KC_F24:
+    if (record->event.pressed) {
+      caps_word_toggle();
+    }
+    return false;
+
   case QK_MODS ... QK_MODS_MAX:
     // Mouse and consumer keys (volume, media) with modifiers work inconsistently across operating systems,
     // this makes sure that modifiers are always applied to the key that was pressed.
